ab94e7505e452b4906a99c6e0a5d07ebbf9b5188
Hugo Valk <huvalk@ttu.ee>
2020-02-15 15:41:22 +0200

    part 5

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -19,12 +19,7 @@ namespace ContosoUniversity.Controllers
         {
             _context = context;
         }
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
